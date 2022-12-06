853c87cdbe8c30793f6ba1ca598ed20a322a7b24
ianrich69420 <ian.orieko@student.moringaschool.com>
2022-12-06 12:52:27 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.querySelector('form').addEventListener("submit", (e) => e.preventDefault())
+  const form = document.querySelector('form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addToDo(e.target.new_task_description.value);
+    e.target.reset();
+  });
+
+  function addToDo (task) {
+    let p = document.createElement("p")
+    p.textContent = `${task}`
+    document.querySelector("#list").appendChild("p")
+  }
 });
